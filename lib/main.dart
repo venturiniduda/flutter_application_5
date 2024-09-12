a6f53e8da3c0f10575b8e3b968281c26c56e1bff
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2024-09-11 22:36:19 -0300

    Primeiras telas

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_5/view/home.dart';
+import 'package:flutter_application_5/view/cadastro.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +17,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'home',
+      routes: {
+        'home': (context) => HomeView(),
+        'cadastro': (context) => CadastroView(),
+      },
     );
   }
 }
